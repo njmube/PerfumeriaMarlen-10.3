ee79d083909fba9b9c9516182e347ed6883b7aea
tracktopell <tracktopell@gmail.com>
2015-05-05 01:54:13 -0500

    fix bugs

--- pmarlen-web-ligth/src/main/java/com/pmarlen/jsf/HistoricoDevolucionesVentaMB.java
+++ pmarlen-web-ligth/src/main/java/com/pmarlen/jsf/HistoricoDevolucionesVentaMB.java
@@ -67,12 +67,11 @@ public class HistoricoDevolucionesVentaMB  {
 		}		
 		return devoluciones;
 	}
-	
-	public String editarDevolucionAction(int devolucionId){
-		logger.config("->editarDevolucionAction:devolucionId="+devolucionId);
-		return editarDevolucionMB.editar(devolucionId);
+	public void editar(int devolucionId){
+		logger.config("->editar:devolucionId="+devolucionId);
+		editarDevolucionMB.editar(devolucionId);
 	}
-	
+
 	public int getSizeList(){
 		logger.fine("->getSizeList()");
 		return getDevoluciones().size();
